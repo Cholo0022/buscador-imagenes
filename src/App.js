ff51d7bac7a79580ba02ec3648343bd92b4bc93a
Cholo0022 <andres.bozzani@gmail.com>
2021-11-09 16:25:49 -0300

    Buscador de imagenes en proceso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import "./Search.css";
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  console.log(photos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID WqCiOYIv4ytYPIJXi9TrIBtRcGQf1IOZEqJiOHMDcig",
+                },
+              }
+            );
+            const data = await response.json();
+            //llamar a api unsplash
+            setPhotos(data.results);
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
     </div>
   );
-}
+};
 
 export default App;
